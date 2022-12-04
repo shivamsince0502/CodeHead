0fba9c2adc3063de54a7276c2c47db15c332280e
Nexus <shubhamkumarsingh7209@gmail.com>
2022-12-04 11:08:35 +0530

    Initial commit

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,123 @@
+import React, { useState, useRef, useEffect } from "react";
+import Client from "../componenets/Client";
+import Editor from "../componenets/Editor";
+import { initSocket } from "../socket";
+import ACTIONS from "../Actions";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import toast from "react-hot-toast";
+
+const EditorPage = () => {
+  const socketRef = useRef(null);
+
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect-error", (err) => handleErrors(err));
+      socketRef.current.on("connect-failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator(-1);
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state.username,
+      });
+
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+      // listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+    };
+    init();
+    // cleaning functions to remove event listeners to prevetn memory sortage
+    return () => {
+      socketRef.current.disconnnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
+  }, []);
+
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success(`Room ID has been copied to your clipboard`);
+    } catch (err) {
+      toast.error("Could not copy the Room ID");
+      console.error(err);
+    }
+  }
+
+  const LeaveRoom = () => {
+    reactNavigator("/");
+  };
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo"></img>
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={LeaveRoom}>
+          Leave
+        </button>
+      </div>
+      <div className="editorWrap">
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
---
+++ src/pages/Home.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
+const Home = () => {
+  const navigate = useNavigate();
+  const[roomId, setRoomId] = useState('')
+  const[username, setUserName] = useState('')
+  const CreateNewRoom = (e)=>{
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomId(id);
+    toast.success('Created a new room');
+  }
+
+  const joinRoom = ()=>{
+    if(!roomId || !username){
+      toast.error('Room ID & username is required')
+      return;
+    }
+
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username, 
+      }
+    })
+
+  }
+
+  const handleInputEnter = (e)=>{
+    if(e.code === 'Enter'){
+      joinRoom();
+    }
+  }
+
+  // console.log(`userName : ${userName}`)
+  // console.log(`userId : ${roomId}`)
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => {
+              setRoomId(e.target.value);
+            }}
+            value={roomId}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            Join
+          </button>
+          <span className="createInfo">
+            If you don't have an invite link then create &nbsp;
+            <a
+              href="https://github.com/shivamsince0502"
+              onClick={CreateNewRoom}
+              className="createNewBtn"
+            >
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Build With ❤️ &nbsp; by &nbsp;
+          <a href="https://adorable-lolly-6c9267.netlify.app/">Shivam Kumar</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
